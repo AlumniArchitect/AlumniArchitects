9902ea89726e62f4f0bea2749574f77c8b6947fc
PrasuHirapara <prasuhirpara@gmail.com>
2025-02-07 09:05:25 +0530

    PH adding chatbot in homepage.jsx from index.html

--- frontend/src/component/HomePage.jsx
+++ frontend/src/component/HomePage.jsx
@@ -19,7 +19,25 @@ export default function Homepage() {
     const showError = (message) => {
         setError(message);
         setTimeout(() => setError(""), 5000);
-      };
+    };
+
+    // useEffect(() => {
+    //     const script1 = document.createElement("script");
+    //     script1.src = "https://cdn.botpress.cloud/webchat/v2.2/inject.js";
+    //     script1.async = true;
+    //     document.body.appendChild(script1);
+
+    //     const script2 = document.createElement("script");
+    //     script2.src = "https://files.bpcontent.cloud/2025/02/07/00/20250207005523-NBGRZXQR.js";
+    //     script2.async = true;
+    //     document.body.appendChild(script2);
+
+    //     return () => {
+    //         document.body.removeChild(script1);
+    //         document.body.removeChild(script2);
+    //     };
+    // }, []);
+
 
     return (
         <>
@@ -27,6 +45,7 @@ export default function Homepage() {
             <div id="homepage">
                 <Navbar />
             </div>
+
         </>
     );
 }
