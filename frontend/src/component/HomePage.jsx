18e3aff11bd18801315193b11f7dcce31fae12a2
PrasuHirapara <prasuhirpara@gmail.com>
2025-02-14 14:52:26 +0530

    PH filtering all pages

--- frontend/src/component/HomePage.jsx
+++ frontend/src/component/HomePage.jsx
@@ -23,22 +23,22 @@ export default function Homepage() {
     setTimeout(() => setError(""), 5000);
   };
 
-  // useEffect(() => {
-  //     const script1 = document.createElement("script");
-  //     script1.src = "https://cdn.botpress.cloud/webchat/v2.2/inject.js";
-  //     script1.async = true;
-  //     document.body.appendChild(script1);
+  useEffect(() => {
+      const script1 = document.createElement("script");
+      script1.src = "https://cdn.botpress.cloud/webchat/v2.2/inject.js";
+      script1.async = true;
+      document.body.appendChild(script1);
 
-  //     const script2 = document.createElement("script");
-  //     script2.src = "https://files.bpcontent.cloud/2025/02/07/00/20250207005523-NBGRZXQR.js";
-  //     script2.async = true;
-  //     document.body.appendChild(script2);
+      const script2 = document.createElement("script");
+      script2.src = "https://files.bpcontent.cloud/2025/02/07/00/20250207005523-NBGRZXQR.js";
+      script2.async = true;
+      document.body.appendChild(script2);
 
-  //     return () => {
-  //         document.body.removeChild(script1);
-  //         document.body.removeChild(script2);
-  //     };
-  // }, []);
+      return () => {
+          document.body.removeChild(script1);
+          document.body.removeChild(script2);
+      };
+  }, []);
 
     return (
         <>
