f72047567ffdc40770b7cb10b841d8fb9f79cea4
fenil3007 <italiyafenil3007@gmail.com>
2025-02-15 10:30:45 +0530

    VT hompage c

--- backend/src/main/java/com/alumniarchitect/controller/algo/Analytics.java
+++ backend/src/main/java/com/alumniarchitect/controller/algo/Analytics.java
@@ -4,10 +4,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-<<<<<<< HEAD
-=======
 @RequestMapping("api/analytics")
->>>>>>> aee2404f1b876ea2356f988588593a6bf117cb40
 public class Analytics {
 
 
